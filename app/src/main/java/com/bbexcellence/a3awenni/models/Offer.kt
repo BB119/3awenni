336a693d792411848964fb93af2ce366b343da9b
Boutheina <bouchahda_boutheina@yahoo.fr>
2022-01-21 22:53:51 +0100

    Added database rules and updated the AppUser class accordingly

--- app/src/main/java/com/bbexcellence/a3awenni/models/Offer.kt
+++ app/src/main/java/com/bbexcellence/a3awenni/models/Offer.kt
@@ -5,7 +5,7 @@ import com.google.firebase.database.IgnoreExtraProperties
 @IgnoreExtraProperties
 data class Offer(
     val id: String? = null,
-    val user: AppUser? = null,
+    val owner: AppUser? = null,
     var title: String? = null,
     var content: String? = null,
     var price: Long? = null,
